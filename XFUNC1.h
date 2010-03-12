9b81dcc84aca853d00cd6918d98f4f546d2084b9
Thomas Braun <thomas.braun@byte-physics.de>
2010-03-12 18:26:42 +0100

    first XOP version copied from XOPFunc1

---
+++ XFUNC1.h
@@ -0,0 +1,10 @@
+/*
+	XFUNC1.h -- equates for XFUNC1 XOP
+*/
+
+/* XFUNC1 custom error codes */
+
+#define REQUIRES_IGOR_200 1 + FIRST_XOP_ERR
+
+/* Prototypes */
+HOST_IMPORT void main(IORecHandle ioRecHandle);

77974f9505f7cec73ecea7aa1221ef4bce6caa93
Thomas Braun <thomas.braun@byte-physics.de>
2010-03-22 18:41:43 +0100

    changes

---
+++ functionBodys.h
@@ -0,0 +1,21 @@
+
+
+static int checkForNewBricklets(checkForNewBrickletsParams *p);
+static int closeResultFile(closeResultFileParams *p);
+static int getAllBrickletData(getAllBrickletDataParams *p);
+static int getAllBrickletMetaData(getAllBrickletMetaDataParams *p);
+static int getBrickletData(getBrickletDataParams *p);
+static int getBrickletMetaData(getBrickletMetaDataParams *p);
+static int getBrickletRawData(getBrickletRawDataParams *p);
+static int getErrorMessage(getErrorMessageParams *p);
+static int getNumberOfBricklets(getNumberOfBrickletsParams *p);
+static int getRangeBrickletData(getRangeBrickletDataParams *p);
+static int getRangeBrickletMetaData(getRangeBrickletMetaDataParams *p);
+static int getResultFileMetaData(getResultFileMetaDataParams *p);
+static int getResultFileName(getResultFileNameParams *p);
+static int getResultFilePath(getResultFilePathParams *p);
+static int getVernissageVersion(getVernissageVersionParams *p);
+static int getXOPVersion(getXOPVersionParams *p);
+static int openResultFile(openResultFileParams *p);
+
+

78bf00765aaaddd202a32c2feca81dbddc7dee1a
Thomas Braun <thomas.braun@byte-physics.de>
2010-11-17 19:41:28 +0100

    various fixes proof read help file and compile it fix Z,I display in preview the new dataview procedure holds all stuff which the individual data windows need implement preliminary windows for 1D and 2D data 2D data also gets the 3pnt bkg substraction from pascal

--- VC8/copyfiles.bat
+++ VC8/copyfiles.bat
@@ -1,9 +1,8 @@
-/*
-	The file copyfiles.bat is part of the "MatrixFileReader XOP".
-	It is licensed under the LGPLv3 with additional permissions,
-	see License.txt	in the source folder for details.
-*/
-
+REM
+REM	The file copyfiles.bat is part of the "MatrixFileReader XOP".
+REM	It is licensed under the LGPLv3 with additional permissions,
+REM see License.txt	in the source folder for details.
+REM
 
 copy Release\matrixfilereader.xop   "I:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"
 copy "..\matrixfilereader Help.ihf" "I:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Help Files"

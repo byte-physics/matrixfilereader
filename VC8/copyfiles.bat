3dde58a9fc934731ae13ce635eaa5a6664bb4d97
unknown <thomas.braun@byte-physics.de>
2010-12-14 16:53:12 +0100

    some changes

--- VC8/copyfiles.bat
+++ VC8/copyfiles.bat
@@ -1,7 +1,7 @@
 REM
 REM	The file copyfiles.bat is part of the "MatrixFileReader XOP".
 REM	It is licensed under the LGPLv3 with additional permissions,
-REM see License.txt	in the source folder for details.
+REM 	see License.txt	in the source folder for details.
 REM
 
 copy Release\matrixfilereader.xop   "I:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"

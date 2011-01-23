49c343d915f96df13b46aff2b4c90940cc8ea86e
Thomas Braun <thomas.braun@byte-physics.de>
2011-01-23 13:31:11 +0100

    adapt to new paths

--- VC8/copyfiles.bat
+++ VC8/copyfiles.bat
@@ -4,5 +4,5 @@ REM	It is licensed under the LGPLv3 with additional permissions,
 REM 	see License.txt	in the source folder for details.
 REM
 
-copy Release\matrixfilereader.xop   "I:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"
-copy "..\matrixfilereader Help.ihf" "I:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Help Files"
+copy Release\matrixfilereader.xop   "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"
+copy "..\matrixfilereader Help.ihf" "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Help Files"

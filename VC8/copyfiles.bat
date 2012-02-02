a308f20125eccc240d2c61407ddbf84b361f5d59
Thomas Braun <thomas.braun@byte-physics.de>
2012-02-02 19:34:34 +0100

    more error checking

--- VC8/copyfiles.bat
+++ VC8/copyfiles.bat
@@ -5,4 +5,17 @@ REM 	see License.txt	in the source folder for details.
 REM
 
 copy Release\matrixfilereader.xop   "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"
+if errorlevel 1 goto BuildEventFailed
+
 copy "..\matrixfilereader Help.ihf" "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Help Files"
+if errorlevel 1 goto BuildEventFailed
+
+REM start H:\gitRepo\Coding\matrix-file-reader\igor-xop\matrixfilereader-basic-gui.pxp
+REM if errorlevel 1 goto BuildEventFailed
+
+goto BuildEventOK
+
+:BuildEventFailed
+exit 1
+
+:BuildEventOK

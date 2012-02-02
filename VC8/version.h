16b7ccacfc40d5bb2e8c6f1cd63e8fb58e8aefcd
Thomas Braun <thomas.braun@byte-physics.de>
2012-02-02 10:36:32 +0100

    raise version

--- VC8/version.h
+++ VC8/version.h
@@ -10,5 +10,5 @@
 	returned by the GetMtrxFileReaderVersion operation and also used for the resource file
 */
 
-#define MatrixFileReader_XOP_VERSION 0,1,8,0
-#define MatrixFileReader_XOP_VERSION_STR "0.18\0"
+#define MatrixFileReader_XOP_VERSION 0,1,8,1
+#define MatrixFileReader_XOP_VERSION_STR "0.181\0"

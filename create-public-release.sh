28063f9380e660cab1f08ea1e99800e45643e2c4
Thomas Braun <thomas.braun@byte-physics.de>
2012-02-03 17:42:46 +0100

    create a zip file and name it properly

--- create-public-release.sh
+++ create-public-release.sh
@@ -7,15 +7,19 @@ if [ ! -z "$(git status -s --untracked-files=no .)" ]; then
 	exit 0
 fi
 
-folder=public-releases/new-release
+lastVersion=0.18
+newVersion=0.19
+
+baseName=matrixFileReaderXOP-v$newVersion
+folder=public-releases/$baseName
 srcFolder=$folder/src
 docFolder=$folder/doc
 pxpFolder=$folder/pxp
 regressFolder=$folder/regression_tests
 
-lastVersion=0.18
-
 rm -rf $folder
+rm -rf $zipfile
+
 mkdir -p $folder
 mkdir -p $srcFolder
 mkdir -p $docFolder
@@ -37,6 +41,7 @@ cp Operation-Template-Generator.pxp $srcFolder
 cp VC8/Release/matrixfilereader.xop $folder
 cp VC8/VC2005_Redist_package_x86/vcredist_x86.exe $folder
 
+
 filesToWatch="VC8 *.txt matrixfilereader-basic-gui.pxp Operation-Template-Generator.pxp *.ihf"
 git rev-parse HEAD > $folder/internalVersionString.txt
 rm -f changelog
@@ -44,3 +49,6 @@ git log --pretty="%B" $(cat public-releases/matrixFileReaderXOP-v$lastVersion/in
 echo "################################################" >> changelog
 echo "################################################" >> changelog
 git log --stat $(cat public-releases/matrixFileReaderXOP-v$lastVersion/internalVersionString.txt)..HEAD $filesToWatch >> changelog
+
+cd public-releases &&  zip -q -r $baseName.zip $baseName/* && cd ..
+

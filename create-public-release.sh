7d27e49b8bae42bd1d5012ed82894473d463c2ea
Thomas Braun <thomas.braun@byte-physics.de>
2014-11-13 19:08:57 +0100

    Move changelog generation to the end as it is currently broken

--- create-public-release.sh
+++ create-public-release.sh
@@ -42,6 +42,7 @@ cp Operation-Template-Generator.pxp $srcFolder
 cp VC8/Release/matrixfilereader.xop $folder
 cp VC8/VC2005_Redist_package_x86/vcredist_x86.exe $folder
 
+cd public-releases &&  zip -q -r $baseName.zip $baseName/* && cd ..
 
 git rev-parse HEAD > $folder/internalVersionString.txt
 rm -f changelog
@@ -49,6 +50,3 @@ git log --pretty="%B" $(cat public-releases/matrixFileReaderXOP-v$lastVersion/in
 echo "################################################" >> changelog
 echo "################################################" >> changelog
 git log --stat $(cat public-releases/matrixFileReaderXOP-v$lastVersion/internalVersionString.txt)..HEAD $filesToWatch >> changelog
-
-cd public-releases &&  zip -q -r $baseName.zip $baseName/* && cd ..
-

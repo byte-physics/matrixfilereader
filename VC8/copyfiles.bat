f379f53771e1c83073ef4c5e981ad3d19203a50e
Thomas Braun <thomas.braun@byte-physics.de>
2020-07-27 20:27:35 +0200

    VC8/copyfiles.bat: Adapt for IP8 32bit
    We can not use start anymore as that defaults to 64bit IP.

--- VC8/copyfiles.bat
+++ VC8/copyfiles.bat
@@ -4,13 +4,13 @@ REM  It is licensed under the LGPLv3 with additional permissions,
 REM   see License.txt in the source folder for details.
 REM
 
-copy Release\matrixfilereader.xop   "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Extensions"
+copy Release\matrixfilereader.xop   "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 8 User Files\Igor Extensions"
 if errorlevel 1 goto BuildEventFailed
 
-copy "..\matrixfilereader Help.ihf" "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 6 User Files\Igor Help Files"
+copy "..\matrixfilereader Help.ihf" "c:\Dokumente und Einstellungen\thomas\Eigene Dateien\WaveMetrics\Igor Pro 8 User Files\Igor Help Files"
 if errorlevel 1 goto BuildEventFailed
 
-start E:\projekte\matrixfilereader\matrixfilereader-basic-gui.pxp
+"C:\Program Files\WaveMetrics\Igor Pro 8 Folder\IgorBinaries_Win32\Igor.exe" E:\projekte\matrixfilereader\matrixfilereader-basic-gui.pxp
 if errorlevel 1 goto BuildEventFailed
 
 goto BuildEventOK

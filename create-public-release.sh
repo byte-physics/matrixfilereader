72f89ba875e3263a7c9906181526117274463ede
Thomas Braun <thomas.braun@byte-physics.de>
2020-07-27 20:26:41 +0200

    VC8/MatrixFileReader.sln: Upgrade to VS 2013

--- create-public-release.sh
+++ create-public-release.sh
@@ -35,7 +35,7 @@ cp matrixfilereader-basic-gui.pxp $pxpFolder
 
 cp regression_tests/*.{bat,log,ipf,pxp} $regressFolder
 
-cp VC8/*.{bat,cpp,h,hpp,rc} VC8/MatrixFileReader.sln VC8/MatrixFileReader.vcproj $srcFolder
+cp VC8/*.{bat,cpp,h,hpp,rc} VC8/MatrixFileReader.sln VC8/MatrixFileReader.vcxproj $srcFolder
 cp -r VC8/bstrlib $srcFolder
 
 cp Operation-Template-Generator.pxp $srcFolder
